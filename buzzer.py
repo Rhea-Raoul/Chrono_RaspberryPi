b92383d310f54fa9d51bc672419cea452a5cd9e3
Rhea-Raoul <rhraoul@salcc.edu.lc>
2025-05-15 00:08:55 -0400

    Comments added for buzzer code

--- buzzer.py
+++ buzzer.py
@@ -1,7 +1,8 @@
+#Import the necessary libraries 
 import RPi.GPIO as GPIO
 import time
 
-
+#Function responsible for initializing the buzzer
 def init_buzzer():
     # Enter buzzer pin
     global buzzer_pin 
@@ -9,7 +10,8 @@ def init_buzzer():
     GPIO.setup(buzzer_pin,GPIO.OUT)
     GPIO.output(buzzer_pin,GPIO.LOW)
 
-
+#Function responsible for beeping the buzzer
+# The buzzer will beep for the time specified in the buzzer_time variable
 def buzzer_beep(buzzer_time):
     GPIO.output(buzzer_pin,GPIO.HIGH)
     time.sleep(buzzer_time)
